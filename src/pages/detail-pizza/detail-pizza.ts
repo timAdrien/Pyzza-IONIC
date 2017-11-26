1834bda55fdc59cba88fde04526991784eb54ffa
timAdrien <timothee.adrien@icloud.com>
2017-11-26 11:00:02 +0100

    go

--- src/pages/detail-pizza/detail-pizza.ts
+++ src/pages/detail-pizza/detail-pizza.ts
@@ -55,14 +55,14 @@ export class DetailPizzaPage {
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
+      encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
     }
 
     this.camera.getPicture(options).then((imageData) => {
       this.pizza.photo.data = imageData;
-      this.pizza.photo.contentType = "image/JPEG";
+      this.pizza.photo.contentType = "image/png";
     }, (err) => {
     });
   }
